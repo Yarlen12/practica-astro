a523de49b41f5a421cde519b43316e7dd913e878
Yarlen <2020030817@upsin.edu.mx>
2025-02-18 18:32:40 -0700

    agregando tailwind config

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import{'tailwindcss'}.config} */
+module.exports = {
+  content: ["./*.{html,js}"],
+  theme: {
+    screens: {
+      sm: "340px",
+      md: "540px",
+      lg: "768px",
+      xl: "1180px",
+    },
+    extend: {},
+    fontFamily: {
+      Lobster: ["Lobster", "serif"],
+    },
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: "12px",
+        md: "32px",
+      },
+    },
+  },
+  plugins: [],
+};
