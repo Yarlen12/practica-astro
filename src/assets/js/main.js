4021f5108d216cd31d2c596b66d13c60087a3f0b
Yarlen <2020030817@upsin.edu.mx>
2025-02-25 22:15:13 -0700

    funcionalidad de scroll up

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -14,6 +14,22 @@ navLink.forEach((link) => {
   });
 });
 
+// SHOW SCROLL UP
+const scrollUp = () => {
+  const scrollUpBtn = document.getElementById("scroll-up");
+
+  if (window.scrollY >= 250) {
+    scrollUpBtn.classList.remove("-bottom-1/2");
+    scrollUpBtn.classList.add("bottom-4");
+  } else {
+    scrollUpBtn.classList.add("-bottom-1/2");
+    scrollUpBtn.classList.remove("bottom-4");
+  }
+};
+
+window.addEventListener("scroll", scrollUp);
+
+// SWIPER
 import Swiper from "swiper/bundle";
 import "swiper/css/bundle";
 
