412d046aa526f0720ea86164d173590f5e6a7611
Yarlen <2020030817@upsin.edu.mx>
2025-02-26 17:06:47 -0700

    funcion nueva para el navbar

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -73,3 +73,31 @@ const swiper = new Swiper(".swiper", {
     },
   },
 });
+
+// SCROLL SECTION
+const activeLink = () => {
+  const sections = document.querySelectorAll("section");
+  const navLink = document.querySelectorAll(".nav-link");
+
+  let current = "home";
+
+  sections.forEach((section) => {
+    const sectionTop = section.offsetTop;
+
+    if (window.scrollY >= sectionTop - 60) {
+      current = section.getAttribute("id");
+    }
+  });
+
+  navLinks.forEach((item) => {
+    item.classList.remove("active");
+
+    if (item.href.includes(current)) {
+      item.classList.add("active");
+    }
+  });
+};
+
+window.addEventListener("scroll", activeLink);
+
+// SCROLL REVEAL ANIMATION
